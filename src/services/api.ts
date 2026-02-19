bdabab39172a53b2c9941e3623a05fef90bdd2f2
« Camille Rahout» <camille.rahout@gmail.com>
2026-02-19 10:24:40 +0100

    created Api fetch function and zod schema

---
+++ src/services/api.ts
@@ -0,0 +1,31 @@
+import { z } from 'zod';
+
+export const ArtworkSchema = z.object({
+    id: z.number(),
+    title: z.string(),
+    artist_title: z.string().optional().default('Unknown Artist'),
+    image_id: z.string().optional().nullable().default(null),
+});
+
+const ApiResponseSchema = z.object({
+  data: z.array(ArtworkSchema)
+});
+
+export type Artwork = z.infer<typeof ArtworkSchema>;
+
+export async function fetchArtworks(query: string){
+    const res = await fetch(`https://api.artic.edu/api/v1/artworks/search?q=${query}&fields=id,title,artist_title,image_id`)
+    if (!res.ok) {
+        throw new Error('Failed to fetch artworks');
+    }
+    const data = await res.json();
+    
+    const result = ApiResponseSchema.safeParse(data);
+    if (!result.success) {
+        throw new Error('Invalid API response');
+    }
+    return result.data.data;
+}
+
+
+
